2d17d45d8d8ad0278d6a182061ebc85190542ebf
Brian Marick <marick@exampler.com>
2014-09-27 17:38:34 -0500

    startup screen love

--- init.el
+++ init.el
@@ -6,8 +6,8 @@
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-(setq inhibit-startup-screen t)
-(setq inhibit-startup-message t)
+; (setq inhibit-startup-screen t)
+; (setq inhibit-startup-message t)
 
 (setenv "EDITOR" "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient")
 
