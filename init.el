332da5c63dfac7b52da99e78a7c8f646451b911d
Brian Marick <marick@exampler.com>
2015-09-08 17:29:43 -0500

    pushed various bits off into healthfinch common

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;;; init.el --- Derived from Milkmacs configuration file
 ;;; http://milkbox.net/note/single-file-master-emacs-configuration/
 
+
 ;; Turn off mouse interface early in startup to avoid momentary display
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
@@ -11,11 +12,7 @@
 
 (setenv "EDITOR" "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient")
 
-(setq require-final-newline t)
-(setq default-mode-line-format
-      '("" "%[%f %*+%* %l (" mode-name minor-mode-alist ")%]" mode-line-process " " mode-line-buffer-identification "%-"))
-
-;;;; Commands that trip me up - or not 
+;; Commands that trip me up - or not 
 
 (put 'downcase-region 'disabled t)
 (put 'upcase-region 'disabled t)
@@ -44,11 +41,8 @@
 (defvar bem-packages 
   '(exec-path-from-shell
     browse-kill-ring
-    ido-ubiquitous
-    ido-vertical-mode
     clojure-mode
     ace-jump-mode
-    ag
     browse-kill-ring
     expand-region
     gist
@@ -56,7 +50,6 @@
     hl-sexp
     markdown-mode+
     rainbow-delimiters
-    smex
     alchemist
     company
     clj-refactor
@@ -81,7 +74,6 @@
       uniquify-ask-about-buffer-names-p t
       uniquify-ignore-buffers-re "^\\*")
 (require 'checkdoc)
-(require 'ido)
 (require 'clojure-jump-to-file)
 
 
@@ -90,7 +82,6 @@
 
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
-(ignore-errors (server-start))
 
 ;;;; macros
 (defmacro after (mode &rest body)
@@ -107,18 +98,6 @@
 (global-auto-revert-mode t)
 (browse-kill-ring-default-keybindings)  ; M-y - n for next, q to quit
 (setq-default backup-directory-alist '(("." . "~/.emacs-backups")))
-
-;;;; recentf
-(recentf-mode t)
-(setq-default recentf-max-menu-items 40)
-(setq-default recentf-menu-filter 'recentf-sort-basenames-ascending)
-
-
-;;;; Darwin
-
-(cond ((eq system-type 'darwin)
-       (setq delete-by-moving-to-trash t)
-       (setq trash-directory "~/.Trash/")))
 
 
 ;;;; GUI settings
@@ -315,42 +294,6 @@ that file in the other window and position point on that line."
 (add-hook 'ruby-mode-hook '(lambda () (hl-line-mode)))
 
 
-;;;; ido
-(ido-mode t)
-(ido-everywhere t)
-(setq ido-enable-flex-matching t)
-(setq ido-auto-merge-work-directories-length nil)
-(setq ido-create-new-buffer 'always)
-(setq ido-everywhere t)
-(setq ido-max-prospects 10)
-(setq ido-read-file-name-non-ido nil)
-(setq ido-use-filename-at-point nil)
-(setq ido-use-virtual-buffers t)
-
-
-(defun mp-ido-hook ()
-  (define-key ido-completion-map (kbd "C-h") 'ido-delete-backward-updir)
-  (define-key ido-completion-map (kbd "C-w") 'ido-delete-backward-word-updir)
-  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
-  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match)
-  (define-key ido-completion-map [tab] 'ido-complete))
-
-(add-hook 'ido-setup-hook 'mp-ido-hook)
-
-
-(after 'ido-ubiquitous-autoloads (ido-ubiquitous-mode t))
-;(after 'ido-ubiquitous (ido-ubiquitous-disable-in evil-ex))
-
-(setq ido-ubiquitous-command-exceptions '(execute-extended-command))
-(setq ido-ubiquitous-function-exceptions '())
-
-(after 'ido-vertical-mode-autoloads
-  (ido-vertical-mode t))
-
-;;;; smex
-
-(add-hook 'after-init-hook 'smex-initialize)
-
 
 ;;;; Markdown mode
 (setq auto-mode-alist
@@ -465,15 +408,6 @@ that file in the other window and position point on that line."
 (global-set-key (kbd "C-h l") 'ace-jump-line-mode)
 (global-set-key (kbd "C-h C-d") 'ace-jump-word-mode)
 
-(global-set-key (kbd "s-P") 'ag-project)
-(global-set-key (kbd "s-p") 'ag-project-at-point)
-
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c M-x") 'smex-update-and-run)
-;; This is your old M-x.
-(global-set-key (kbd "M-z") 'execute-extended-command)
-
 (global-set-key (kbd "H-w") 'er/expand-region)
 
 (global-set-key (kbd "H-c") 'ns-copy-including-secondary)
@@ -503,9 +437,12 @@ that file in the other window and position point on that line."
 (global-set-key "\^h\^h" 'ruby-visit-source)    
 
 
+;;; Healthfinch
+(push "~/h/emacs-common-denominator" load-path)
+(require 'healthfinch-init)
 
 
-
+(ignore-errors (server-start))
 ;;; init.el ends here
 
 (custom-set-variables
