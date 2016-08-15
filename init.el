2f32809c9872c5c9af3a9ccca0ddef30b289467a
Brian Marick <marick@exampler.com>
2016-08-15 10:46:24 -0500

    `elixir-visit-source`

--- init.el
+++ init.el
@@ -197,6 +197,44 @@
 
 
 ;;;; elixir
+
+(defun elixir-visit-source ()
+  "If the current line contains text like 'old_procedure_controller_test.exs:57', visit 
+that file in the other window and position point on that line."
+  (interactive)
+  (let* ((start-boundary (save-excursion (beginning-of-line) (point)))
+         (regexp (concat "\\([ \t\n\r\"'([<{]\\|^\\)" ; non file chars or
+                                                      ; effective
+                                                      ; beginning of file  
+                         "\\(.+\\.e+xs?\\):\\([0-9]+\\)")) 
+         (matchp (save-excursion
+                  (end-of-line)
+                  ;; if two matches on line, the second is most likely
+                  ;; to be useful, so search backward.
+                  (re-search-backward regexp start-boundary t))))
+    (cond (matchp
+           (let ((file (buffer-substring (match-beginning 2)
+                                         (match-end 2))) 
+                 (line (buffer-substring (match-beginning 3)
+                                         (match-end 3))))
+             ; Windows: Find-file doesn't seem to work with Cygwin
+             ; //<drive>/ format or the odd /cygdrive/<drive>/ format 
+             (if (or (string-match "//\\(.\\)\\(.*\\)" file)
+                     (string-match "/cygdrive/\\(.\\)\\(.*\\)" file))
+                 (setq file
+                       (concat (substring file
+                                          (match-beginning 1)
+                                          (match-end 1))
+                               ":"
+                               (substring file
+                                          (match-beginning 2)
+                                          (match-end 2)))))
+                             
+             (find-file-other-window file)
+             (goto-line (string-to-int line))))
+          (t
+           (error "No elixir location on line.")))))
+
 (defun my-elixir-mode-hook ()
   (alchemist-mode 1)
   (company-mode 1))
@@ -433,6 +471,7 @@ that file in the other window and position point on that line."
 (global-set-key (kbd "H-m") 'lein-shell-command-again)
 
 (global-set-key "\^h\^h" 'ruby-visit-source)    
+(global-set-key "\^he" 'elixir-visit-source)    
 
 
 ;;; Healthfinch
