1f0fc383c122a9d6e1bc20fa2efc3bb8d8b75a5a
Brian Marick <marick@exampler.com>
2016-09-19 14:03:16 -0500

    Add flycheck for elm / tweak elm-mode

--- libs/marick-elm.el
+++ libs/marick-elm.el
@@ -4,6 +4,22 @@
 
 (use-package elm-mode
   :ensure t
-  :init (add-hook 'elm-mode-hook #'elm-oracle-setup-completion))
+  :init
+  (add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
+  (add-to-list 'company-backends 'company-elm)
+  ; (elm-indent-mode false)
+  (setq elm-indent-offset 2))
+             
+
+(use-package flycheck
+  :init (global-flycheck-mode)
+  :ensure t)
+
+;; (use-package flycheck-elm
+;;   :ensure t
+;;   :init (add-hook 'flycheck-mode-hook #'flycheck-elm-setup)
+;;         (add-hook 'after-init-hook #'global-flycheck-mode))
+
 
 (provide 'marick-elm)
+;;; marick-elm.el ends here
